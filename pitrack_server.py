4d268e09e62bc819247138291911e1ef13d2f012
Tim Bytnar <tbytnar@gmail.com>
2020-08-22 10:50:42 -0500

    print fixes

--- pitrack_server.py
+++ pitrack_server.py
@@ -6,9 +6,12 @@ from time import sleep  # import
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
 # Bind the socket to the port
-server_address = ('localhost', 10000)
+server_address = 'localhost'
+port = 10000
 print >>sys.stderr, 'starting up on %s port %s' % server_address
+print('starting up on ' + server_address + ' port ' + port)
 sock.bind(server_address)
 
 # some MPU6050 Registers and their Address
@@ -68,11 +71,11 @@ sock.listen(1)
 
 while True:
     # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
+    print('waiting for a connection')
     connection, client_address = sock.accept()
 
     try:
-        print >>sys.stderr, 'connection from', client_address
+        print('connection from: ' + client_address)
 
         # Receive the data in small chunks and retransmit it
         while True:
